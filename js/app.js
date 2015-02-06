2e184a82129c3c85658375354c6e3c6f6799026f
Javier Enrique <s0wkete@gmail.com>
2015-02-06 14:02:25 -0300

    Base for the graphs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 $(document).foundation();
+
+var graph=d3.select(document.getElementById('create-graph'));
+
+start();
+
+function onClick1() {
+    deselect();
+    graph.attr("class","selectedRadial");
+}
+
+function labelFunction(val,min,max) {}
+
+function deselect() {
+    graph.attr("class","radial");
+}
+
+function start() {
+
+    var rp1 = radialProgress(document.getElementById('create-graph'))
+            // .label("RADIAL 1")
+            .onClick(onClick1)
+            .diameter(350)
+            .value(75)
+            .render();
+
+}
